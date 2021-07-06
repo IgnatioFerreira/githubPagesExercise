095eaaf7354e5ff1db2e9dca4d71831f73507546
Ignatio Ferreira <79564877+IgnatioFerreira@users.noreply.github.com>
2021-07-06 16:33:06 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,14 +10,14 @@
 var drops = [];
 
 function setup() {
-  createCanvas(640, 360);
+  createCanvas(2000, 1500);
   for (var i = 0; i < 500; i++) {
     drops[i] = new Drop();
   }
 }
 
 function draw() {
-  background(230, 230, 250);
+  background(300, 300, 300);
   for (var i = 0; i < drops.length; i++) {
     drops[i].fall();
     drops[i].show();
